44b708df63c02c6d9d5d965f09ba9859200fed1e
chitrang <chitrang200889@gmail.com>
2021-08-17 17:59:48 -0400

    setup for mysql

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,8 +75,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '',
+        'PORT': 3306,
+        'NAME': 'mysitedb',
+        'USER': 'chitrang',
+        'PASSWORD': 'Test@123',
     }
 }
 
